3e688f5ba28724c4159ed4be86bca4f059f6de97
Karel Nagel <nagelkarel@gmail.com>
2025-11-13 20:27:32 +0200

    converting to mp4 working

--- src/templates/Main.tsx
+++ src/templates/Main.tsx
@@ -1,9 +1,10 @@
-import { AbsoluteFill, CalculateMetadataFunction } from 'remotion'
+import { AbsoluteFill, CalculateMetadataFunction, Html5Video } from 'remotion'
 import { z } from 'zod'
 import { api } from '../api'
-import { HlsVideo } from './HlsVideo'
 import { Files, RouteSegment } from '../api/types'
 import { createQCameraStreamUrl } from '../api/route'
+import { FFmpeg } from '@ffmpeg/ffmpeg'
+import { fetchFile, toBlobURL } from '@ffmpeg/util'
 
 const FPS = 30
 
@@ -19,6 +20,21 @@ export const MainProps = z.object({
 })
 export type MainProps = z.infer<typeof MainProps>
 
+const convert = async (file: string) => {
+  const ffmpeg = new FFmpeg()
+  const baseURL = 'https://cdn.jsdelivr.net/npm/@ffmpeg/core@0.12.10/dist/umd'
+  ffmpeg.on('log', ({ message }) => console.log(message))
+  await ffmpeg.load({
+    coreURL: await toBlobURL(`${baseURL}/ffmpeg-core.js`, 'text/javascript'),
+    wasmURL: await toBlobURL(`${baseURL}/ffmpeg-core.wasm`, 'application/wasm'),
+  })
+  await ffmpeg.writeFile('input.hevc', await fetchFile(file))
+  await ffmpeg.exec(['-r', '20', '-i', 'input.hevc', '-c', 'copy', '-map', '0', '-vtag', 'hvc1', 'output.mp4'])
+  const data = await ffmpeg.readFile('output.mp4')
+  const url = URL.createObjectURL(new Blob([(data as any).buffer], { type: 'video/mp4' }))
+  return url
+}
+
 export const calculateMetadata: CalculateMetadataFunction<MainProps> = async ({ props: { routeName } }) => {
   const [dongleId] = routeName.split('/')
 
@@ -31,11 +47,14 @@ export const calculateMetadata: CalculateMetadataFunction<MainProps> = async ({ 
   const files = await api.file.files.query({ params: { routeName: routeName.replace('/', '%7C') } })
   if (files.status !== 200) throw new Error('Failed getting files!')
 
+  const url = await convert(files.body.dcameras[0])
+
   const data = {
     segments: segments.body,
     files: files.body,
-    qCamUrl,
+    qCamUrl: url,
   }
+
   return {
     durationInFrames: durationInSeconds * FPS,
     props: { routeName, data },
@@ -45,7 +64,7 @@ export const calculateMetadata: CalculateMetadataFunction<MainProps> = async ({ 
 export const Main = (props: MainProps) => {
   return (
     <AbsoluteFill style={{ backgroundColor: 'red', justifyContent: 'center', alignItems: 'center', fontSize: 50 }}>
-      {props.data && <HlsVideo src={props.data.qCamUrl} style={{ width: '100%' }} />}
+      {props.data && <Html5Video src={props.data.qCamUrl} style={{ width: '100%' }} />}
     </AbsoluteFill>
   )
 }
