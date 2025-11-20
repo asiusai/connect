654585475290ffc1093a6d8bd8406599fc0c4e85
Karel Nagel <nagelkarel@gmail.com>
2025-11-20 17:46:13 +0200

    cleaner

--- log-reader/src/reader.ts
+++ log-reader/src/reader.ts
@@ -1,18 +1,20 @@
 import { CapnpStream } from './stream'
-import { Event as EventWrapper } from './event'
 import { Event } from './geval'
 import { StreamSelector } from './stream-selector'
+import capnp from 'capnp-ts'
+import * as Log from '../capnp/log.capnp'
+import { toJSON } from './capnp-json'
 
-export type StreamReaderOptions = {
-  isBinary?: boolean
-}
+export const eventToJSON = (buf: Buffer, struct?: any) => toJSON(new capnp.Message(buf, false).getRoot(Log.Event), struct)
+
+export type StreamReaderOptions = { isBinary?: boolean }
 export const StreamReader = (inputStream: StreamSelector, options: StreamReaderOptions = {}) => {
   const event = Event()
   const capnpStream = new CapnpStream()
   let isStarted = false
 
   capnpStream.on('message', (buf) => {
-    if (!options.isBinary) event.broadcast(EventWrapper(buf).toJSON())
+    if (!options.isBinary) event.broadcast(eventToJSON(buf))
     else event.broadcast(buf)
   })
 
