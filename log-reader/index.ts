faf2d32370124fcb4bf811dc960415606a687b3c
Karel <nagelkarel@gmail.com>
2025-11-21 10:50:28 +0200

    Log reader (#3)
    * reader working
    
    * converted to ts
    
    * fix types
    
    * cleaner
    
    * capnp ts files
    
    * working with new capnp files
    
    * update and one file
    
    * web reader
    
    * perfect
    
    * cleanup
    
    * remove src folder

---
+++ log-reader/index.ts
@@ -0,0 +1,191 @@
+import * as capnp from 'capnp-ts'
+import * as Log from './capnp/log.capnp'
+import { Decompress } from 'fzstd'
+
+// --- Types ---
+
+type CapnpStructClass = {
+  _capnp: { displayName: string }
+  [key: string]: any
+}
+
+// --- Helpers ---
+
+const toJSON = (capnpObject: any, struct?: CapnpStructClass): any => {
+  if (typeof capnpObject !== 'object' || !capnpObject._capnp) return capnpObject
+  if (Array.isArray(capnpObject)) return capnpObject.map((x) => toJSON(x))
+  if (capnpObject.constructor._capnp.displayName.startsWith('List')) {
+    return capnpObject.toArray().map((n: any) => toJSON(n))
+  }
+
+  if (!struct) struct = capnpObject.constructor as CapnpStructClass
+
+  const which = capnpObject.which ? capnpObject.which() : -1
+  const data: Record<string, any> = {}
+  const proto = Object.getPrototypeOf(capnpObject)
+
+  Object.getOwnPropertyNames(proto).forEach((method) => {
+    if (!method.startsWith('get')) return
+    const name = method.substr(3)
+    let capsName = ''
+    let wasLower = false
+
+    for (let i = 0; i < name.length; ++i) {
+      if (name[i].toLowerCase() !== name[i]) {
+        if (wasLower) capsName += '_'
+        wasLower = false
+      } else wasLower = true
+      capsName += name[i].toUpperCase()
+    }
+
+    if (which !== -1 && struct![capsName] !== undefined && which !== struct![capsName]) return
+
+    Object.defineProperty(data, name, {
+      enumerable: true,
+      configurable: true,
+      get: () => {
+        let value = capnpObject[method]()
+
+        if (value?.constructor) {
+          const typeName = value.constructor.name
+          if (typeName === 'Uint64' || typeName === 'Int64') {
+            value = value.toString()
+          } else if (typeName === 'Data') {
+            const uint8 = value.toUint8Array()
+            let binary = ''
+            const len = uint8.byteLength
+            for (let i = 0; i < len; i++) binary += String.fromCharCode(uint8[i])
+            value = btoa(binary)
+          } else if (typeName === 'Pointer') {
+            try {
+              let dataArr = capnp.Data.fromPointer(value).toUint8Array()
+              if (dataArr.byteLength > 0 && dataArr[dataArr.byteLength - 1] === 0) {
+                dataArr = dataArr.subarray(0, dataArr.byteLength - 1)
+              }
+              value = new TextDecoder().decode(dataArr)
+            } catch {
+              value = undefined
+            }
+          } else {
+            value = toJSON(value)
+          }
+        }
+
+        Object.defineProperty(data, name, {
+          configurable: false,
+          writable: false,
+          value: value,
+        })
+        return value
+      },
+    })
+  })
+
+  return data
+}
+
+const getMessageSize = (view: DataView): number | null => {
+  if (view.byteLength < 8) return null
+  const segmentCount = view.getUint32(0, true) + 1
+  const headerSize = 4 + segmentCount * 4
+  const paddedHeaderSize = headerSize + (headerSize % 8 === 0 ? 0 : 8 - (headerSize % 8))
+  if (view.byteLength < paddedHeaderSize) return null
+
+  let totalBodySize = 0
+  for (let i = 0; i < segmentCount; i++) {
+    const segmentWords = view.getUint32(4 + i * 4, true)
+    totalBodySize += segmentWords * 8
+  }
+  return paddedHeaderSize + totalBodySize
+}
+
+// --- Streaming Logic ---
+
+const ZSTD_MAGIC = new Uint8Array([0x28, 0xb5, 0x2f, 0xfd])
+
+const createZstdDecompressor = () => {
+  let decompressor: Decompress
+
+  return new TransformStream<Uint8Array, Uint8Array>({
+    start: (controller) => {
+      decompressor = new Decompress((chunk) => controller.enqueue(chunk))
+    },
+    transform: (chunk) => decompressor.push(chunk),
+    flush: () => decompressor.push(new Uint8Array(0), true),
+  })
+}
+
+const getSmartLogStream = async (inputStream: ReadableStream<Uint8Array>): Promise<ReadableStream<Uint8Array>> => {
+  const reader = inputStream.getReader()
+  const { value: firstChunk, done } = await reader.read()
+
+  if (done) {
+    reader.releaseLock()
+    return new ReadableStream()
+  }
+
+  let isZstd = false
+  if (firstChunk.length >= 4) {
+    isZstd =
+      firstChunk[0] === ZSTD_MAGIC[0] &&
+      firstChunk[1] === ZSTD_MAGIC[1] &&
+      firstChunk[2] === ZSTD_MAGIC[2] &&
+      firstChunk[3] === ZSTD_MAGIC[3]
+  }
+
+  const stitchedStream = new ReadableStream<Uint8Array>({
+    start: (controller) => {
+      controller.enqueue(firstChunk)
+    },
+    pull: async (controller) => {
+      try {
+        const { value, done } = await reader.read()
+        if (done) controller.close()
+        else controller.enqueue(value)
+      } catch (e) {
+        controller.error(e)
+      }
+    },
+    cancel: (reason) => {
+      reader.cancel(reason)
+    },
+  })
+
+  if (isZstd) return stitchedStream.pipeThrough(createZstdDecompressor())
+  return stitchedStream
+}
+
+export async function* LogReader(stream: ReadableStream<Uint8Array>): AsyncGenerator<any, void, unknown> {
+  stream = await getSmartLogStream(stream)
+  const reader = stream.getReader()
+  let buffer = new Uint8Array(0)
+
+  try {
+    while (true) {
+      const { done, value } = await reader.read()
+      if (done) break
+
+      const temp = new Uint8Array(buffer.length + value.length)
+      temp.set(buffer)
+      temp.set(value, buffer.length)
+      buffer = temp
+
+      while (true) {
+        const view = new DataView(buffer.buffer, buffer.byteOffset, buffer.byteLength)
+        const msgSize = getMessageSize(view)
+
+        if (!msgSize || buffer.byteLength < msgSize) break
+
+        const msgBytes = buffer.subarray(0, msgSize)
+        const msgCopy = new Uint8Array(msgBytes)
+        const message = new capnp.Message(msgCopy, false)
+        const event = message.getRoot(Log.Event)
+        yield toJSON(event)
+
+        buffer = buffer.subarray(msgSize)
+      }
+    }
+  } finally {
+    reader.releaseLock()
+  }
+}
