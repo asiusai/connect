d44a95231da486730f48f18322d6b50d5b342fe4
Karel Nagel <nagelkarel@gmail.com>
2025-11-25 13:31:12 +0200

    init

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -1,3 +1,31 @@
+import { useState } from 'react'
+import { hevcToMp4 } from '../utils/ffmpeg'
+import { useAsyncEffect } from '../utils/hooks'
+
+const HEVC_URL = '/fcamera.hevc'
+
+export const Current = () => {
+  const [url, setUrl] = useState<string>()
+  useAsyncEffect(async () => {
+    const bin = await fetch(HEVC_URL).then((x) => x.arrayBuffer())
+    const blob = await hevcToMp4(new Uint8Array(bin), console.log)
+    setUrl(URL.createObjectURL(blob))
+  }, [])
+
+  if (!url) return null
+  return <video src={url} controls autoPlay />
+}
+
+export const Streaming = () => {
+  // TODO
+  return null
+}
+
 export const Component = () => {
-  return <div>test</div>
+  return (
+    <div>
+      <Current />
+      <Streaming />
+    </div>
+  )
 }
