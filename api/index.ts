5c88af46cca2dc70d8f87ed10bd520ebcad93e14
Karel Nagel <nagelkarel@gmail.com>
2025-11-28 20:35:47 +0200

    port 8080

--- api/index.ts
+++ api/index.ts
@@ -3,6 +3,7 @@ import { renderer } from '../src/api/contract'
 import { router } from './router'
 
 const server = Bun.serve({
+  port: 8080,
   fetch: async (request) => {
     const res = await fetchRequestHandler({
       contract: renderer,
