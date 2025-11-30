92625b27c87fc8f8b5c8b24b3eaac569567f9069
Karel Nagel <nagelkarel@gmail.com>
2025-11-30 13:31:08 +0200

    timeout

--- api/index.ts
+++ api/index.ts
@@ -11,6 +11,7 @@ const headers = {
 
 const server = Bun.serve({
   port: 8080,
+  idleTimeout: 600,
   fetch: async (request) => {
     try {
       if (request.method === 'OPTIONS') return new Response(null, { headers })
