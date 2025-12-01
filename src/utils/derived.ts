9db673b36aae40d376fff943e7ab5995855c86d2
Karel Nagel <nagelkarel@gmail.com>
2025-12-01 12:07:03 +0200

    cleaner derived

--- src/utils/derived.ts
+++ src/utils/derived.ts
@@ -1,7 +1,7 @@
 import type { Route } from '../types'
 import { getRouteDuration } from '../utils/format'
 
-export interface GPSPathPoint {
+export type GPSPathPoint = {
   t: number
   lng: number
   lat: number
@@ -9,66 +9,27 @@ export interface GPSPathPoint {
   dist: number
 }
 
-interface IDriveEvent {
+type OpenpilotState = 'disabled' | 'preEnabled' | 'enabled' | 'softDisabling' | 'overriding'
+type AlertStatus = 0 | 1 | 2
+
+type DriveEvent = {
   type: string
   time: number
   offset_millis: number
   route_offset_millis: number
   data: object
-}
+} & (
+  | { type: 'event'; data: { event_type: 'record_front_toggle' | 'first_road_camera_frame' } }
+  | { type: 'state'; data: { state: OpenpilotState; enabled: boolean; alertStatus: AlertStatus } }
+  | { type: 'user_flag'; data: Record<string, never> }
+)
 
-type EventDriveEvent = IDriveEvent & {
-  type: 'event'
-  data: {
-    event_type: 'record_front_toggle' | 'first_road_camera_frame'
-  }
-}
-
-type OpenpilotState = 'disabled' | 'preEnabled' | 'enabled' | 'softDisabling' | 'overriding'
-
-type AlertStatus = 0 | 1 | 2
-
-type StateDriveEvent = IDriveEvent & {
-  type: 'state'
-  data: {
-    state: OpenpilotState
-    enabled: boolean
-    alertStatus: AlertStatus
-  }
-}
-
-type UserFlagDriveEvent = IDriveEvent & {
-  type: 'user_flag'
-  data: Record<string, never>
-}
-
-type DriveEvent = EventDriveEvent | StateDriveEvent | UserFlagDriveEvent
-
-type EngagedTimelineEvent = {
-  type: 'engaged'
-  route_offset_millis: number
-  end_route_offset_millis: number
-}
-
-type AlertTimelineEvent = {
-  type: 'alert'
-  route_offset_millis: number
-  end_route_offset_millis: number
-  alertStatus: AlertStatus
-}
-
-type OverridingTimelineEvent = {
-  type: 'overriding'
-  route_offset_millis: number
-  end_route_offset_millis: number
-}
-
-type UserFlagTimelineEvent = {
-  type: 'user_flag'
-  route_offset_millis: number
-}
-
-export type TimelineEvent = EngagedTimelineEvent | AlertTimelineEvent | OverridingTimelineEvent | UserFlagTimelineEvent
+export type TimelineEvent = { route_offset_millis: number } & (
+  | { type: 'engaged'; end_route_offset_millis: number }
+  | { type: 'alert'; end_route_offset_millis: number; alertStatus: AlertStatus }
+  | { type: 'overriding'; end_route_offset_millis: number }
+  | { type: 'user_flag' }
+)
 
 export interface RouteStatistics {
   routeDurationMs: number
@@ -108,26 +69,21 @@ const generateTimelineEvents = (route: Route, events: DriveEvent[]): TimelineEve
 
   // convert events to timeline events
   const res: TimelineEvent[] = []
-  let lastEngaged: StateDriveEvent | undefined
-  let lastAlert: StateDriveEvent | undefined
-  let lastOverride: StateDriveEvent | undefined
+  let lastEngaged: Extract<DriveEvent, { type: 'state' }> | undefined
+  let lastAlert: Extract<DriveEvent, { type: 'state' }> | undefined
+  let lastOverride: Extract<DriveEvent, { type: 'state' }> | undefined
 
   const isOverriding = (state: OpenpilotState) => ['overriding', 'preEnabled'].includes(state)
 
   events.forEach((ev) => {
-    if (ev.type === 'state') {
+    if (ev.type === 'user_flag') res.push({ type: 'user_flag', route_offset_millis: ev.route_offset_millis })
+    else if (ev.type === 'state') {
       const { enabled, alertStatus, state } = ev.data
       if (lastEngaged && !enabled) {
-        res.push({
-          type: 'engaged',
-          route_offset_millis: lastEngaged.route_offset_millis,
-          end_route_offset_millis: ev.route_offset_millis,
-        } satisfies EngagedTimelineEvent)
+        res.push({ type: 'engaged', route_offset_millis: lastEngaged.route_offset_millis, end_route_offset_millis: ev.route_offset_millis })
         lastEngaged = undefined
       }
-      if (!lastEngaged && enabled) {
-        lastEngaged = ev
-      }
+      if (!lastEngaged && enabled) lastEngaged = ev
 
       if (lastAlert && lastAlert.data.alertStatus !== alertStatus) {
         res.push({
@@ -135,55 +91,37 @@ const generateTimelineEvents = (route: Route, events: DriveEvent[]): TimelineEve
           route_offset_millis: lastAlert.route_offset_millis,
           end_route_offset_millis: ev.route_offset_millis,
           alertStatus: lastAlert.data.alertStatus,
-        } satisfies AlertTimelineEvent)
+        })
         lastAlert = undefined
       }
-      if (!lastAlert && alertStatus !== 0) {
-        lastAlert = ev
-      }
+      if (!lastAlert && alertStatus !== 0) lastAlert = ev
 
       if (lastOverride && !isOverriding(ev.data.state)) {
         res.push({
           type: 'overriding',
           route_offset_millis: lastOverride.route_offset_millis,
           end_route_offset_millis: ev.route_offset_millis,
-        } satisfies OverridingTimelineEvent)
+        })
         lastOverride = undefined
       }
-      if (!lastOverride && isOverriding(state)) {
-        lastOverride = ev
-      }
-    } else if (ev.type === 'user_flag') {
-      res.push({
-        type: 'user_flag',
-        route_offset_millis: ev.route_offset_millis,
-      })
+      if (!lastOverride && isOverriding(state)) lastOverride = ev
     }
   })
 
   // ensure events have an end timestamp
-  if (lastEngaged) {
-    res.push({
-      type: 'engaged',
-      route_offset_millis: lastEngaged.route_offset_millis,
-      end_route_offset_millis: routeDuration,
-    })
-  }
-  if (lastAlert) {
+  if (lastEngaged)
+    res.push({ type: 'engaged', route_offset_millis: lastEngaged.route_offset_millis, end_route_offset_millis: routeDuration })
+
+  if (lastAlert)
     res.push({
       type: 'alert',
       route_offset_millis: lastAlert.route_offset_millis,
       end_route_offset_millis: routeDuration,
       alertStatus: lastAlert.data.alertStatus,
     })
-  }
-  if (lastOverride) {
-    res.push({
-      type: 'overriding',
-      route_offset_millis: lastOverride.route_offset_millis,
-      end_route_offset_millis: routeDuration,
-    })
-  }
+
+  if (lastOverride)
+    res.push({ type: 'overriding', route_offset_millis: lastOverride.route_offset_millis, end_route_offset_millis: routeDuration })
 
   return res
 }
