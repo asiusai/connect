6bd463069516cffc12dd0420a19326acea6194e0
Karel Nagel <nagelkarel@gmail.com>
2025-12-01 10:04:43 +0200

    proxy fix?

--- athena-proxy.js
+++ athena-proxy.js
@@ -1,19 +1,19 @@
 const TARGET_DOMAIN = 'https://athena.comma.ai'
 
+const headers = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Methods': '*',
+  'Access-Control-Allow-Headers': 'Content-Type, User-Agent, Authorization',
+  'Access-Control-Max-Age': '86400',
+}
 export default {
   fetch: async (request) => {
-    if (request.method === 'OPTIONS') return new Response(null, { status: 204, headers: CORS_HEADERS })
+    if (request.method === 'OPTIONS') return new Response(null, { status: 204, headers })
 
     const url = new URL(request.url)
 
     const response = await fetch(new Request(TARGET_DOMAIN + url.pathname + url.search, request))
 
-    const newResponse = new Response(response.body, response)
-    newResponse.headers.set('Access-Control-Allow-Origin', '*')
-    newResponse.headers.set('Access-Control-Allow-Methods', '*')
-    newResponse.headers.set('Access-Control-Allow-Headers', 'Content-Type, User-Agent, Authorization')
-    newResponse.headers.set('Access-Control-Max-Age', '86400')
-
-    return newResponse
+    return new Response(response.body, { ...response, headers: { ...response.headers, ...headers } })
   },
 }
