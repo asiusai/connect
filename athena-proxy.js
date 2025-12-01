cdd48b976fd511b1c9c16260ed56081a1ad257f6
Karel Nagel <nagelkarel@gmail.com>
2025-12-01 10:09:25 +0200

    maybe now?

--- athena-proxy.js
+++ athena-proxy.js
@@ -1,19 +1,23 @@
 const TARGET_DOMAIN = 'https://athena.comma.ai'
 
-const headers = {
+const HEADERS = {
   'Access-Control-Allow-Origin': '*',
   'Access-Control-Allow-Methods': '*',
   'Access-Control-Allow-Headers': 'Content-Type, User-Agent, Authorization',
   'Access-Control-Max-Age': '86400',
 }
+
 export default {
   fetch: async (request) => {
-    if (request.method === 'OPTIONS') return new Response(null, { status: 204, headers })
+    if (request.method === 'OPTIONS') return new Response(null, { status: 204, headers: HEADERS })
 
     const url = new URL(request.url)
 
     const response = await fetch(new Request(TARGET_DOMAIN + url.pathname + url.search, request))
 
-    return new Response(response.body, { ...response, headers: { ...response.headers, ...headers } })
+    const newResponse = new Response(response.body, response)
+    for (const key in HEADERS) newResponse.headers.set(key, HEADERS[key])
+
+    return newResponse
   },
 }
