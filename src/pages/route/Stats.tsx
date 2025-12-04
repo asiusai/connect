43c83afae45d6512275d404b03e92e2659f230f6
Karel <nagelkarel@gmail.com>
2025-12-04 20:05:57 +0200

    OG images (#14)
    * init
    
    * og images looking okay

--- src/pages/route/Stats.tsx
+++ src/pages/route/Stats.tsx
@@ -1,29 +1,21 @@
 import clsx from 'clsx'
-import { useState, useEffect } from 'react'
-import { RouteStatistics, getTimelineEvents, generateRouteStatistics } from '../../utils/derived'
 import { formatDistance, formatDuration, formatRouteDuration } from '../../utils/format'
 import { Route } from '../../types'
-
-const formatEngagement = (stats?: RouteStatistics) =>
-  !stats?.routeDurationMs ? undefined : `${(100 * (stats.engagedDurationMs / stats.routeDurationMs)).toFixed(0)}%`
-
-const useTimelineEvents = (route: Route) => {
-  const [stats, setStats] = useState<RouteStatistics>()
-  useEffect(() => {
-    getTimelineEvents(route).then((x) => setStats(generateRouteStatistics(route, x)))
-  }, [route])
-  return stats
-}
+import { useAsyncMemo } from '../../utils/hooks'
+import { getRouteStats } from '../../utils/derived'
 
 export const Stats = ({ className, route }: { className?: string; route: Route }) => {
-  const stats = useTimelineEvents(route)
+  const stats = useAsyncMemo(() => getRouteStats(route), [route])
 
   return (
     <div className={clsx('flex w-full justify-between gap-8 p-5 bg-background-alt rounded-xl', className)}>
       {[
         { label: 'Distance', value: formatDistance(route?.distance) },
         { label: 'Duration', value: stats ? formatDuration(stats.routeDurationMs / (60 * 1000)) : formatRouteDuration(route) },
-        { label: 'Engaged', value: formatEngagement(stats) },
+        {
+          label: 'Engaged',
+          value: stats?.routeDurationMs ? `${(100 * (stats.engagedDurationMs / stats.routeDurationMs)).toFixed(0)}%` : undefined,
+        },
       ]?.map((stat) => (
         <div key={stat.label} className="flex basis-0 grow flex-col justify-between">
           <span className="text-sm text-background-alt-x">{stat.label}</span>
--- templates/Root.tsx
+++ templates/Root.tsx
@@ -1,26 +1,40 @@
 import '../src/index.css'
-import { Composition } from 'remotion'
+import { Composition, Still } from 'remotion'
 import { FPS, HEIGHT, WIDTH } from './shared'
 import { Preview, previewCalculateMetadata } from './Preview'
 import { PreviewProps } from '../src/types'
 import { env } from '../src/utils/env'
+import { OG, ogCalculateMetadata, OGProps } from './OG'
 
 export const RemotionRoot = () => {
   return (
-    <Composition
-      id="Preview"
-      component={Preview}
-      durationInFrames={100}
-      fps={FPS}
-      width={WIDTH}
-      height={HEIGHT}
-      schema={PreviewProps}
-      calculateMetadata={previewCalculateMetadata}
-      defaultProps={{
-        routeName: env.EXAMPLE_ROUTE_NAME,
-        largeCameraType: 'cameras',
-        smallCameraType: 'dcameras',
-      }}
-    />
+    <>
+      <Composition
+        id="Preview"
+        component={Preview}
+        durationInFrames={100}
+        fps={FPS}
+        width={WIDTH}
+        height={HEIGHT}
+        schema={PreviewProps}
+        calculateMetadata={previewCalculateMetadata}
+        defaultProps={{
+          routeName: env.EXAMPLE_ROUTE_NAME,
+          largeCameraType: 'cameras',
+          smallCameraType: 'dcameras',
+        }}
+      />
+      <Still
+        id="OG"
+        component={OG}
+        height={630}
+        width={1200}
+        schema={OGProps}
+        calculateMetadata={ogCalculateMetadata}
+        defaultProps={{
+          routeName: env.EXAMPLE_ROUTE_NAME,
+        }}
+      />
+    </>
   )
 }
