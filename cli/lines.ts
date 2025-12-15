8494c042c08f96380103f6846e487f092853eeae
Karel Nagel <nagelkarel@gmail.com>
2025-12-15 17:10:39 +0200

    line limit

--- cli/lines.ts
+++ cli/lines.ts
@@ -82,7 +82,7 @@ if (import.meta.main) {
     const totalLines = files.reduce((acc, it) => acc + it.lines, 0)
     console.log('Total lines:', totalLines)
 
-    if (totalLines > 6000) {
+    if (totalLines > 10000) {
       console.warn('Exceeded line limit!')
       process.exit(1)
     } else {
