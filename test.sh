f67b44ddb36b0e71ea73134ff2ede0f27b3c18a2
Karel Nagel <nagelkarel@gmail.com>
2025-12-29 12:50:13 +0200

    fix tsc

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@ set -e
 bun install --frozen-lockfile
 bun biome ci
 [ -z "$SKIP_PLAYWRIGHT_INSTALL" ] && bun playwright install
-# bun run tsc # capnp-ts package fails 
+bun run tsc
 # bun run test run
 bun scripts/lines.ts
 bun scripts/bundle-size.ts
