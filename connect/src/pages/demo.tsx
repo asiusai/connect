960b6d8d8e68cd1e1d3be738e58694a82436d219
Karel Nagel <nagelkarel@gmail.com>
2026-01-12 18:33:12 +0200

    demo dongleit

--- connect/src/pages/demo.tsx
+++ connect/src/pages/demo.tsx
@@ -1,8 +1,11 @@
 import { Navigate } from 'react-router-dom'
 import { setAccessToken } from '../utils/helpers'
 import { env } from '../utils/env'
+import { useStorage } from '../utils/storage'
 
 export const Component = () => {
   setAccessToken(env.DEMO_ACCESS_TOKEN)
+  const [val, set] =useStorage("lastDongleId")
+  set(env.DEMO_DONGLE_ID)
   return <Navigate to="/" />
 }
